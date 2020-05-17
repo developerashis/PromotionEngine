96345d807b5860f52517c9ca5c6f7114f41a4226
Ashis Nandi <AshisNandi.Nandi@resideo.com>
2020-05-17 14:25:06 +0530

    Add project files.

---
+++ PromotionEngine.Lib/PromotionEngine.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PromotionEngine.Lib
+{
+    public class PromotionEngine
+    {
+    }
+}
---
+++ PromotionEngine.Test/PromotionEngineTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PromotionEngine.Test
+{
+    public class PromotionEngineTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
